4a23d4bd151f25ce17d43fd77f621cc8292b7e72
spartan-readytocode <7007966033abc@gmail.com>
2025-09-14 01:41:03 +0530

    feat : added oauth, todo : add signin and signup page

--- v1/src/lib/auth.ts
+++ v1/src/lib/auth.ts
@@ -8,4 +8,11 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", 
     }),
+    socialProviders: {
+    google: {
+      prompt: "select_account", 
+      clientId: process.env.GOOGLE_CLIENT_ID as string, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+    }, 
+  }, 
 });
