e5c95475fae3ae83eb6bc1729d257e94926c2eaf
spartan-readytocode <7007966033abc@gmail.com>
2025-09-14 13:19:10 +0530

    feat : added new prisma schema

--- v1/src/lib/auth.ts
+++ v1/src/lib/auth.ts
@@ -1,6 +1,5 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
 import { PrismaClient } from "@/generated/prisma";
 
 const prisma = new PrismaClient();
